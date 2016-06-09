46cbc7422912295fdadea48b40684b44f3b7cba9
Aleksi Mustonen <aleksi.mustonen@mystes.fi>
2016-06-09 10:59:41 +0300

    A draft of future test proxy

--- src/test/integration/java/io/aino/agents/core/TestProxy.java
+++ src/test/integration/java/io/aino/agents/core/TestProxy.java
@@ -16,67 +16,68 @@ public class TestProxy {
         this.destinationPort = destinationPort;
     }
 
-    private void run() throws IOException {
-        ServerSocket ss = new ServerSocket(sourcePort);
+    private void runProxy() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(sourcePort);
 
         final byte[] request = new byte[1024];
         byte[] reply = new byte[4096];
 
-        while(isRunning()) {
-            Socket client = null, server = null;
+        while(running) {
+            Socket client = serverSocket.accept();
+            Socket server = new Socket(host, destinationPort);
+
             try {
-                client = ss.accept();
+                proxyThread(client, server, request).start();
 
-                final InputStream from_client = client.getInputStream();
-                final OutputStream to_client= client.getOutputStream();
-
-                try { server = new Socket(host, destinationPort); }
-                catch (IOException e) {
-                    PrintWriter out = new PrintWriter(new OutputStreamWriter(to_client));
-                    out.println("Proxy server cannot connect to " + host + ":" +
-                            destinationPort + ":\n" + e);
-                    out.flush();
-                    client.close();
-                    continue;
-                }
-
-                final InputStream from_server = server.getInputStream();
-                final OutputStream to_server = server.getOutputStream();
-
-                Thread t = new Thread() {
-                    public void run() {
-                        int bytes_read;
-                        try {
-                            while((bytes_read = from_client.read(request)) != -1) {
-                                to_server.write(request, 0, bytes_read);
-                                to_server.flush();
-                            }
-                        }
-                        catch (IOException e) {}
-                        try {to_server.close();} catch (IOException e) {}
-                    }
-                };
-
-                t.start();
-                int bytes_read;
+                final OutputStream clientOut= client.getOutputStream();
+                final InputStream serverIn = server.getInputStream();
                 try {
-                    while((bytes_read = from_server.read(reply)) != -1) {
-                        to_client.write(reply, 0, bytes_read);
-                        to_client.flush();
-                    }
+                    readBytes(reply, serverIn, clientOut);
+                } finally {
+                    clientOut.close();
                 }
-                catch(IOException e) {}
-                to_client.close();
             }
-            catch (IOException e) { System.err.println(e); }
             finally {
                 try {
                     if (server != null) server.close();
                     if (client != null) client.close();
                 }
-                catch(IOException e) {}
+                catch(IOException e) {
+                    //don't care, just a test thing
+                }
             }
         }
+    }
+
+    private Thread proxyThread(Socket client, Socket server, final byte[] request) throws IOException{
+        final InputStream clientIn = client.getInputStream();
+        final OutputStream serverOut = server.getOutputStream();
+
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    readBytes(request, clientIn, serverOut);
+                }
+                catch (IOException e) {
+                    //don't care, just a test thing
+                }
+                try {
+                    serverOut.close();
+                } catch (IOException e) {
+                    //don't care, just a test thing
+                }
+            }
+        };
+        return thread;
+    }
+
+    private int readBytes(final byte[] request, InputStream inputStream, OutputStream outputStream) throws IOException {
+        int amountOfBytes;
+        while((amountOfBytes = inputStream.read(request)) != -1) {
+            outputStream.write(request, 0, amountOfBytes);
+            outputStream.flush();
+        }
+        return amountOfBytes;
     }
 
     public boolean isRunning(){
@@ -85,7 +86,15 @@ public class TestProxy {
 
     public void start() throws Exception {
         this.running = true;
-        run();
+        new Thread(){
+            public void run(){
+                try {
+                    runProxy();
+                } catch (IOException e){
+
+                }
+            }
+        }.run();
     }
 
     public void stop() {
